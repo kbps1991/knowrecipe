bedbfdc48d68f7c609d88d76b2a116466b408079
Kiran Balaji <kiran.balaji@cerner.com>
2019-12-22 22:25:36 +0530

    updates

---
+++ src/navigation/about.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../App.css'
+
+const About = () => {
+    return (
+        <div className="about-box">
+            <h1>About Food Recipe</h1>
+            <p>
+                A recipe is a set of instructions that describes how to prepare or make something, especially a dish of prepared food.
+                The term recipe is also used in medicine or in information technology (e.g., user acceptance). A doctor will usually begin a prescription with recipe, Latin for take, usually abbreviated as Rx or the equivalent symbol (℞).
+                Contents
+                1	History
+                1.1	Early examples
+                1.2	Modern recipes and cooking advice
+                2	Components
+                3	Internet and television recipes
+                4	Recipe design tools
+                5	See also
+                6	References
+                7	External links
+                History
+                Early examples
+                Apicius, De re culinaria, an early collection of recipes.
+                The earliest known written recipes date from approximately 1600 BC and come from an Akkadian tablet from southern Babylonia.[1] There are also works in ancient Egyptian hieroglyphs depicting the preparation of food.[2]
+                Many ancient Greek recipes are known. Mithaecus's cookbook was an early one, but most of it has been lost; Athenaeus quotes one short recipe in his Deipnosophistae. Athenaeus mentions many other cookbooks, all of them lost.[3]
+                Roman recipes are known starting in the 2nd century BCE with Cato the Elder's De Agri Cultura. Many authors of this period described eastern Mediterranean cooking in Greek and in Latin.[3] Some Punic recipes are known in Greek and Latin translation.[3]
+                The large collection of recipes De re coquinaria, conventionally titled Apicius, appeared in the 4th or 5th century and is the only complete surviving cookbook from the classical world.[3] It lists the courses served in a meal as Gustatio (appetizer), Primae Mensae (main course) and Secundae Mensae (dessert).[4] Each recipe begins with the Latin command "Take...," "Recipe...."[5]
+                Arabic recipes are documented starting in the 10th century; see al-Warraq and al-Baghdadi.
+                The earliest recipe in Persian dates from the 14th century. Several recipes have survived from the time of Safavids, including Karnameh (1521) by Mohammad Ali Bavarchi, which includes the cooking instruction of more than 130 different dishes and pastries, and Madat-ol-Hayat (1597) by Nurollah Ashpaz.[6] Recipe books from the Qajar period are numerous, the most notable being Khorak-ha-ye Irani by prince Nader Mirza.[7]
+                King Richard II of England commissioned a recipe book called Forme of Cury in 1390,[8] and around the same time, another book was published entitled Curye on Inglish, "cury" meaning cooking.[9] Both books give an impression of how food for the noble classes was prepared and served in England at that time. The luxurious taste of the aristocracy in the Early Modern Period brought with it the start of what can be called the modern recipe book. By the 15th century, numerous manuscripts were appearing detailing the recipes of the day. Many of these manuscripts give very good information and record the re-discovery of many herbs and spices including coriander, parsley, basil and rosemary, many of which had been brought back from the Crusades.[10]
+            </p>
+        </div>
+    );
+}
+
+export default About
---
+++ src/navigation/services.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const Services = () => <h1>Our services</h1>
+export default Services
