672334c4b7b3bda2f573998c9990da02301f3490
Kiran Balaji <kiran.balaji@cerner.com>
2020-05-02 15:34:40 +0530

    Updates

---
+++ precache-manifest.59dabeef5c53bca21c900fd880987580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1caf8dc9ce3f9d79dff566efb810580",
+    "url": "/knowrecipe/index.html"
+  },
+  {
+    "revision": "408b163eb3ca350deefe",
+    "url": "/knowrecipe/static/css/main.48d6116d.chunk.css"
+  },
+  {
+    "revision": "824a1ac7032522dc48a7",
+    "url": "/knowrecipe/static/js/2.3d78fba9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/knowrecipe/static/js/2.3d78fba9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "408b163eb3ca350deefe",
+    "url": "/knowrecipe/static/js/main.601f9885.chunk.js"
+  },
+  {
+    "revision": "27a1af49f04bb4d2cb17",
+    "url": "/knowrecipe/static/js/runtime-main.e746c646.js"
+  },
+  {
+    "revision": "8325a79fd55fb77da55eb3c19d748569",
+    "url": "/knowrecipe/static/media/logo.8325a79f.png"
+  }
+]);
