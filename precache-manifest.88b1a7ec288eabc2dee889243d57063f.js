3e53ce290a5ff631e0420d8b21acec6e0db6991b
Kiran Balaji <kiran.balaji@cerner.com>
2020-05-02 15:56:05 +0530

    Updates

---
+++ precache-manifest.88b1a7ec288eabc2dee889243d57063f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9718c9bcbf0c084fa2da147ba6037ccc",
+    "url": "/knowrecipe/index.html"
+  },
+  {
+    "revision": "4f4fc4fef39814cbbc01",
+    "url": "/knowrecipe/static/css/main.b4afba1e.chunk.css"
+  },
+  {
+    "revision": "824a1ac7032522dc48a7",
+    "url": "/knowrecipe/static/js/2.3d78fba9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/knowrecipe/static/js/2.3d78fba9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f4fc4fef39814cbbc01",
+    "url": "/knowrecipe/static/js/main.0de7e059.chunk.js"
+  },
+  {
+    "revision": "27a1af49f04bb4d2cb17",
+    "url": "/knowrecipe/static/js/runtime-main.e746c646.js"
+  },
+  {
+    "revision": "8325a79fd55fb77da55eb3c19d748569",
+    "url": "/knowrecipe/static/media/logo.8325a79f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad0df03cd2936ead6560823c08e6d9b8.js"
+  "/knowrecipe/precache-manifest.88b1a7ec288eabc2dee889243d57063f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/knowrecipe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
